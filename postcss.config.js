f438e0ce1e4a4ee4c35d7598851aaaf52281aa0c
nazarko <me@nazarko.dev>
2025-04-22 04:48:40 +0300

    fixing invalid styles matching after build

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,10 @@
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import prefixWrap from 'postcss-prefixwrap';
 
 const config = {
 	plugins: [
 		tailwindcss(),
-		autoprefixer(),
-		prefixWrap('.shadcn-theme-editor', {
-			whitelist: ['.shadcn-theme-editor']
-		})
+		autoprefixer()
 	]
 }
 
